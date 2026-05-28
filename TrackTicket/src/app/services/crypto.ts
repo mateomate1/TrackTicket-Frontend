f94eb0f8c93f000a3547d37484a270e21d35084a
devTighadouini <iiliaastighadouini@gmail.com>
2026-05-28 13:14:07 +0200

    feat(cypto): cambio de webCrypto por node-forge para tener compatibilidad con PKCS1-v1.5

--- TrackTicket/src/app/services/crypto.ts
+++ TrackTicket/src/app/services/crypto.ts
@@ -2,91 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { enviroment } from '@shared/environmentAPI';
+import * as forge from 'node-forge';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class Crypto {
-  
-    private publikey: CryptoKey | null = null;
-    private readonly REFRESH_INTERNAL_MS = 15 * 60 * 1000;
 
-    constructor(private httpClient: HttpClient) {
-      this.loadPublicKey();
-      setInterval(() => this.loadPublicKey(), this.REFRESH_INTERNAL_MS);
+  private publicKey: forge.pki.PublicKey | null = null;
+  private readonly REFRESH_INTERVAL_MS = 15 * 60 * 1000;
 
-    }
+  constructor(private httpClient: HttpClient) {
+    this.loadPublicKey();
+    setInterval(() => this.loadPublicKey(), this.REFRESH_INTERVAL_MS);
+  }
 
-    private async loadPublicKey(): Promise<void> {
-
-      try {
-        const base64Key = await firstValueFrom (
-          this.httpClient.post(
-            enviroment.root + enviroment.publicKey,
-            null, 
-            { 
-              responseType: 'text'
-            }
-          )
-        );
-
-        const binKey = this.base64ToArrayBuffer(base64Key);
-
-        this.publikey = await crypto.subtle.importKey(
-          'spki',
-          binKey, 
-          {
-            name: 'RSA-OAEP', hash: 'SHA-256'
-          },
-          false,
-          ['encrypt']
-        );
-
-        console.log('Clave pública cargada correctamente');
-      } catch(error) {
-
-        console.error('Error al cargar la clave pública:', error);
-      }
-
-    }
-
-    private base64ToArrayBuffer(base64: string): ArrayBuffer {
-      const binaryString = atob(base64);
-      const bytes = new Uint8Array(binaryString.length);
-
-      for(let i = 0; i < binaryString.length; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-      }
-
-      return bytes.buffer;
-    }
-   
-    async encrypt(text: string): Promise<String> {
-      if (!this.publikey) {
-        throw new Error('La clave publica aún no está cargada');
-
-      }
-
-      const encoded = new TextEncoder().encode(text);
-
-      const encryptedBuffer = await crypto.subtle.encrypt(
-        { name: 'RSA-OAEP' },
-        this.publikey,
-        encoded
-
+  private async loadPublicKey(): Promise<void> {
+    try {
+      const base64Key = await firstValueFrom(
+        this.httpClient.post(
+          enviroment.root + enviroment.publicKey,
+          null,
+          { responseType: 'text' }
+        )
       );
 
-      return this.arrayBufferToBase64(encryptedBuffer);
+      const derKey = forge.util.decode64(base64Key);
+      const asn1 = forge.asn1.fromDer(derKey);
+      this.publicKey = forge.pki.publicKeyFromAsn1(asn1);
+
+      console.log('Clave pública cargada correctamente');
+    } catch (error) {
+      
+      console.error('Error al cargar la clave pública:', error);
+    }
+  }
+
+  encrypt(text: string): string {
+    if (!this.publicKey) {
+      throw new Error('La clave pública aún no está cargada');
     }
 
-    private arrayBufferToBase64(buffer: ArrayBuffer): string {
-      const bytes = new Uint8Array(buffer);
-      let binary = '';
-      for (let i = 0; i < bytes.byteLength; i++) {
-        binary += String.fromCharCode(bytes[i]);
-      }
-
-      return btoa(binary);
-    }
+    const encrypted = (this.publicKey as any).encrypt(text, 'RSAES-PKCS1-V1_5');
+    return forge.util.encode64(encrypted);
+  }
 }
