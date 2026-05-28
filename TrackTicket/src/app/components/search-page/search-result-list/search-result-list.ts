fc1c4399f2619d2ab287e9730cd5c45ca30f134d
mateomate1 <mateoayarrabarbero@gmail.com>
2026-05-28 13:11:55 +0200

    create listSeect method to center map on venue

--- TrackTicket/src/app/components/search-page/search-result-list/search-result-list.ts
+++ TrackTicket/src/app/components/search-page/search-result-list/search-result-list.ts
@@ -1,5 +1,7 @@
+import { searchResponse } from '@interfaces/concert-search-response';
 import { Component, inject } from '@angular/core';
 import { ConcertSearch } from '@services/concert-search';
+import { MapService } from '@services/map-service';
 
 @Component({
   selector: 'search-result-list',
@@ -9,5 +11,11 @@ import { ConcertSearch } from '@services/concert-search';
   styleUrl: './search-result-list.css',
 })
 export class SearchResultList {
-  concertSearch = inject(ConcertSearch)
+  concertSearch = inject(ConcertSearch);
+  mapa = inject(MapService);
+
+  listClick(site: searchResponse) {
+    this.mapa.center.set({ lat: site.venue.latitude, lng: site.venue.longitude });
+    this.mapa.zoom.set(16);
+  }
 }
