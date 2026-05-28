153ed20f19a70444467f3d5cde3e7fbaa98a5b7b
mateomate1 <mateoayarrabarbero@gmail.com>
2026-05-28 13:55:48 +0200

    final changes

--- TrackTicket/src/app/components/search-page/search-result-list/search-result-list.ts
+++ TrackTicket/src/app/components/search-page/search-result-list/search-result-list.ts
@@ -1,6 +1,9 @@
 import { searchResponse } from '@interfaces/concert-search-response';
-import { Component, inject } from '@angular/core';
+
+import { Component, inject, signal } from '@angular/core';
+
 import { ConcertSearch } from '@services/concert-search';
+
 import { MapService } from '@services/map-service';
 
 @Component({
@@ -14,8 +17,29 @@ export class SearchResultList {
   concertSearch = inject(ConcertSearch);
   mapa = inject(MapService);
 
+  loading = signal(false);
+
+  animateZoom(targetZoom: number) {
+    const interval = setInterval(() => {
+      const currentZoom = this.mapa.zoom();
+      if (currentZoom >= targetZoom) {
+        clearInterval(interval);
+        this.loading.set(false);
+        return;
+      }
+      this.mapa.zoom.set(currentZoom + 1);
+    }, 80);
+  }
+
   listClick(site: searchResponse) {
-    this.mapa.center.set({ lat: site.venue.latitude, lng: site.venue.longitude });
-    this.mapa.zoom.set(16);
+    this.loading.set(true);
+    this.mapa.zoom.set(5);
+    setTimeout(() => {
+      this.mapa.center.set({
+        lat: site.venue.latitude,
+        lng: site.venue.longitude,
+      });
+      this.animateZoom(16);
+    }, 500);
   }
 }
