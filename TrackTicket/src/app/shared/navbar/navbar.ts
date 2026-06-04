9115d301cadd17211a0890b54d79a2537df527d0
mateomate1 <mateoayarrabarbero@gmail.com>
2026-06-04 16:41:02 +0200

    final commit

--- TrackTicket/src/app/shared/navbar/navbar.ts
+++ TrackTicket/src/app/shared/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { User } from '@services/user';
 import { Auth } from '@services/auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -14,12 +15,29 @@ export class Navbar {
   private auth = inject(Auth);
   private router = inject(Router);
 
+  showUserMenu = false;
+
   goToAuth(): void {
     this.router.navigate(['/auth']);
   }
 
   async logout(): Promise<void> {
     await this.auth.logout();
+  }
+
+  async confirmDeleteAccount() {
+    const result = await Swal.fire({
+      title: 'Borrar cuenta',
+      text: 'Esta accion no se puede deshacer',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, borrar',
+      cancelButtonText: 'Cancelar',
+    });
+
+    if (result.isConfirmed) {
+      await this.deleteAccount();
+    }
   }
 
   async deleteAccount(): Promise<void> {
