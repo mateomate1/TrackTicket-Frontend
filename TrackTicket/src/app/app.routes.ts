bf186a48990f47385120d98dc71875aa660cda16
mateomate1 <mateoayarrabarbero@gmail.com>
2026-06-05 12:16:28 +0200

    update

--- TrackTicket/src/app/app.routes.ts
+++ TrackTicket/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Favorites } from './pages/favorites/favorites';
 import { SearchPage } from './pages/search-page/search-page';
 import { AuthPage } from './pages/auth-page/auth-page';
 import { authGuard } from './guards/auth-guard';
+import { ConcertPage } from '@pages/concert-page/concert-page';
 export const routes: Routes = [
   {
     path: 'search',
@@ -16,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'artists/:nombre/:genero',
     component: ArtistProfile,
+  },
+  {
+    path: 'concert/:id',
+    component: ConcertPage,
   },
   {
     path: 'auth',
