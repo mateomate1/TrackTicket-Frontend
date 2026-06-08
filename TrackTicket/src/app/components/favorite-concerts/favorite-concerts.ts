3ebf752df67cbcc7c58d867eb25339226ac8a7e4
mateomate1 <mateoayarrabarbero@gmail.com>
2026-06-08 08:55:38 +0200

    update

--- TrackTicket/src/app/components/favorite-concerts/favorite-concerts.ts
+++ TrackTicket/src/app/components/favorite-concerts/favorite-concerts.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FavoritesService } from '@services/favorites/favorites-service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { searchResponse } from '@interfaces/concert-search-response';
 
 @Component({
   selector: 'app-favorite-concerts',
@@ -8,10 +9,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './favorite-concerts.html',
   styleUrl: './favorite-concerts.css',
 })
-export class FavoriteConcerts implements OnInit{
+export class FavoriteConcerts implements OnInit {
   favService = inject(FavoritesService);
+  router = inject(Router);
 
   ngOnInit(): void {
     this.favService.loadFavConcerts();
+  }
+
+  concertDetailClick(concert: searchResponse) {
+    this.router.navigate(['/concert', concert.idTicketMaster]);
   }
 }
